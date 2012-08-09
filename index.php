fdfebf15fe43b4f49c241f759c207019ce71747a
Ben Saletta <redeye229@aol.com>
2012-08-08 20:43:15 -0700

    I guess the last commit didn't work...

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <!DOCTYPE HTML>
 <head>
 	<title>Charity Chain</title>
-</head>
-	
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>	
 <body>
-	
-	
+	<div id="cover">
+		<h1>Charity Chain</h1>
+		<h3>Development hosted by <a href="http://www.github.com">github</a></h3>
+		<img src="img/Octocat.png" id="octocat" onclick="stupid()">
+		
+	</div>
+	<script type="text/javascript" src="script/script.js"></script>
 </body>
